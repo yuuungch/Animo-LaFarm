226fae653bc6b2047a286e6a0dacb979eae7d75f
ArigatYungch <105798512+ArigatYungch@users.noreply.github.com>
2022-10-15 21:54:20 +0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 public class Driver {
     public static void main(String[] args) {
-        Player player = new Player();
+
+        ArrayList<Integer> seedInv = new ArrayList<Integer>();
+        Seeds seedData = new Seeds();
+        Exp expData = new Exp();
+        Player player = new Player(seedInv, seedData, expData);
         Tile tile = new Tile();
         Farm farm = new Farm(player, tile);
 
