553ba48af929272fae39e72916657803937a155e
ArigatYungch <105798512+ArigatYungch@users.noreply.github.com>
2022-11-05 16:13:28 +0800

    Added comments for navigation

--- Player.java
+++ Player.java
@@ -14,6 +14,9 @@ public class Player {
         expData = new Exp();
     }
 
+    /**
+     * This method serves to initialize the seed inventory of the player
+     */
     public void InitializeInventory() {
         int i;
 
@@ -22,14 +25,27 @@ public class Player {
         }
     }
 
-    public void CheckExpBonus() {
-        expData.CheckStatus();
-    }
-
+    /**
+     * Check Player's Seed Inventory
+     */
     public void CheckSeedInventory() {
+        int i;
+        Seeds printTemp = new Seeds();
+
+        System.out.print("Seed Names: [");
+
+        for (i = 1; i <= 7; i++) {
+            printTemp.Generate(i);
+            System.out.print(printTemp.getName() + ", ");
+        }
+        printTemp.Generate(i);
+        System.out.println(printTemp.getName() + "]");
         System.out.println("Seed Inventory : " + seedInv);
     }
 
+    /**
+     * Menu for buying seeds
+     */
     public void BuySeeds() {
         Scanner input = new Scanner(System.in);
         int choice, type, prev, quantity;
@@ -46,6 +62,8 @@ public class Player {
             System.out.println("--Fruit Trees--");
             System.out.println("[7] Mango");
             System.out.println("[8] Apple");
+            System.out.println("----------------");
+            System.out.println("[0] Exit");
 
             System.out.print("\nWhat seed would you like to buy: ");
             choice = input.nextInt();
@@ -64,13 +82,15 @@ public class Player {
                                     + expData.getCostReduction() + " Objectcoins deduction already applied)");
                 System.out.print("How many would you like to buy? ");
                 quantity = input.nextInt();
-                if (Ocoins >= seedData.getCost() * quantity) {
+                if (Ocoins >= seedData.getCost() * quantity && quantity != 0) {
                     prev = seedInv.get(type - 1);
-                    seedInv.set(type - 1, prev + quantity); // tentative
-                    System.out.println("Purchase successful! You now have " + seedInv.get(type - 1) + " pieces of "
+                    seedInv.set(type - 1, prev + quantity);
+                    System.out.println("Purchase successful! You now have " + seedInv.get(type - 1) + " piece/s of "
                             + seedData.getName() + ".");
                     Ocoins -= (seedData.getCost() * quantity);
                     System.out.println("You now have " + Ocoins + " Objectcoins left.");
+                } else if (quantity == 0) {
+                    System.out.println("Please enter a value greater than 0 to buy.");
                 } else {
                     System.out.println("Sorry. You do not have enough Object Coins to purchase that many.");
                 }
@@ -95,8 +115,10 @@ public class Player {
             } else if (choice == 8) {
                 // seedData.Generate(choice);
                 System.out.println("Out of stock. Sorry.");
+            } else if (choice != 0) {
+                System.out.println("Invalid choice. Try again.");
             }
-        } while (choice != 0 || (choice < 0 && choice > 9));
+        } while (choice != 0);
     }
 
     public ArrayList<Integer> getSeedInv() {
