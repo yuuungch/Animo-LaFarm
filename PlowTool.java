ff2c22969999b31f8884fd02142fd0bc9654d7e1
ArigatYungch <105798512+ArigatYungch@users.noreply.github.com>
2022-11-07 21:23:50 +0800

    Update PlowTool.java

--- PlowTool.java
+++ PlowTool.java
@@ -1,13 +1,6 @@
 import java.util.ArrayList;
 
 public class PlowTool {
-    private Exp playerExp;
-
-    
-    public PlowTool(Exp playerExp) {
-        this.playerExp = playerExp;
-    }
-
 
     /**
      * Method for plowing a tile (To be moved to Hoe class)
@@ -19,12 +12,12 @@ public class PlowTool {
         if (array.get(x).getPlowState() == 0 && array.get(x).getRockState() != 0) {
             array.get(x).setPlowState(1);
             System.out.println("Tile has been successfully plowed!");
-            playerExp.GainExp(0.5, array.get(x).getPlayerExp());
+            array.get(x).getPlayerExp().GainExp(0.5, array.get(x).getPlayerExp());
         } else if (array.get(x).getPlowState() == 1) {
             System.out.println("Tile is already plowed!");
         } else if (array.get(x).getPlowState() == 0 && array.get(x).getRockState() == 0) {
             System.out.println("Tile cannot be plowed as there are rocks on it.");
         }
     }
-    
+
 }
